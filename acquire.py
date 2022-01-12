4d8beb8366481e9fc8054adc0513e8666338eab9
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2022-01-12 10:48:55 -0600

    testing acquire link

---
+++ acquire.py
@@ -0,0 +1,3 @@
+Module created to make work from Time Series >> Acquire Lesson reproducible. Link to exercises is <a href=https://github.com/Jonessn22/time-series-exercises/blob/8a63149aa59a46e633cc70e609fbd76b4a649808/01b_acquisition_exercises.ipynb>here</a>. 
+
+[here](https://github.com/Jonessn22/time-series-exercises/blob/8a63149aa59a46e633cc70e609fbd76b4a649808/01b_acquisition_exercises.ipynb)
